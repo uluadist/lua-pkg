cb8f9f998707310affec7bc8f59a01d0865f7dba
Stefano Peluchetti <phd.st.p@gmail.com>
2015-11-25 19:01:16 +0000

    Prepare for release v1.0-beta7.

--- __meta.lua
+++ __meta.lua
@@ -1,12 +1,12 @@
 return {
   name = "pkg",
-  version = "1.0.beta6",
+  version = "1.0.beta7",
   require = {
     luajit = "2.0",
     cURL = "0.3.1",
     lfs  = "1.6.2",
   },
   license = "MIT <http://opensource.org/licenses/MIT>",
-  homepage = "http://scilua.org/pkg.html",
+  homepage = "http://ulua.io/pkg.html",
   description = "ULua package manager",
 }
