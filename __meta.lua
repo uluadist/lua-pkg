43871e82dedca179e8e179fa5c27d6ea6d62f49d
Stefano Peluchetti <phd.st.p@gmail.com>
2015-11-30 20:32:27 +0100

    Prepare for v1.0.0-beta8.

--- __meta.lua
+++ __meta.lua
@@ -1,10 +1,11 @@
 return {
   name = "pkg",
-  version = "1.0.beta7",
+  version = "1.0.beta8",
   require = {
     luajit = "2.0",
     cURL = "0.3.1",
     lfs  = "1.6.2",
+    serpent = "0.27",
   },
   license = "MIT <http://opensource.org/licenses/MIT>",
   homepage = "http://ulua.io/pkg.html",
